5b066403df8a670e91520cab5cb605303a8506d9
mohamed reda <ouyoussefmohamedreda2@gmail.com>
2026-05-04 18:08:38 +0100

    Starte real building

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# setup and imports
+import click
+from rich.console import Console
+from commands.search import search_files
+from commands.organize import organize_folder
+from commands.move import move_file
+from commands.remove import remove_file
+from commands.read import read_content
+from commands.read_line import read_specific_line
+from commands.create import create_file
+from config.settings import APP_NAME, VERSION
+
+console = Console()
+
 # Title: REDAX CLI
 Title_REDAX_CLI = ["██████╗ ███████╗██████╗  █████╗ ██╗  ██╗     ██████╗██╗     ██╗",
                    "██╔══██╗██╔════╝██╔══██╗██╔══██╗╚██╗██╔╝    ██╔════╝██║     ██║",
@@ -10,5 +24,5 @@ Title_REDAX_CLI = ["██████╗ ███████╗████�
 # run
 if __name__ == "__main__":
     for i in range(len(Title_REDAX_CLI)):
-        print(Title_REDAX_CLI[i])
-    print("\nWelcome to REDAX CLI! Please enter your command:")
+        console.print(Title_REDAX_CLI[i])
+    console.print("\nWelcome to REDAX CLI! Please enter your command:")
